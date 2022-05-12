6b1aa403a1a27d7e8ef2ff06dc1371c631cd9f35
TzuHsienChiang <happxia@gmail.com>
2022-05-12 09:41:40 +0800

    3-2 製作 login page、其他分頁

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const express = require('express');
 const app = express();
 
 // middleware
-app.use((req, res, next) => {
+app.use((req, res, next) => { //app.use也是middleware，他們不是處理請求，而是權限，會寫在app.get前面，因為app.get執行完就會結束(res end)
 	console.log('Hello!');
     next();
 });
@@ -21,16 +21,31 @@ app.use((req, res, next) => {
     next();
 });
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { //app.get也是middleware，是處理請求回應
     // res.writeHead(200, { 'Content-Type': 'text/html' });
     // res.write('<head><meta charset="utf-8" /></head>')
     // res.write('<body>')
     // res.write('<h1>這是首頁</h1>')
-    // res.write('</body>')
-    res.status(200)
+    // res.write('</body>') //node.js原生做法
+    res.status(200) //網頁狀態碼 200代表請求成功 404就是fail
         .sendFile(path.join(__dirname, 'views', 'index.html'));
+app.get('/login', (req, res) => {
+    res.status(200)
+        .sendFile(path.join(__dirname, 'views', 'login.html'));
+    });
+app.get('/introduction', (req, res) => {
+    res.status(200)
+        .sendFile(path.join(__dirname, 'views', 'introduction.html'));
+    });
+
+    
+        
 });
 
 app.listen(3000, () => {
 	console.log('Web Server is running on port 3000');
+});
+app.get('/login', (req, res) => {
+    res.status(200)
+        .sendFile(path.join(__dirname, 'views', 'login.html'));
 });

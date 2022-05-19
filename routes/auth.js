682061716f5617f148534d10f52fc005f71edf5c
TzuHsienChiang <happxia@gmail.com>
2022-05-19 10:57:50 +0800

    5-1 重構 auth 路由 (分為 route, controller)

--- routes/auth.js
+++ routes/auth.js
@@ -3,30 +3,43 @@
 //回到app.js import並使用
 
 const express = require('express');
+
+const authController = require('../controllers/auth'); //對照git hub 5-1 
+
+
 const router = express.Router();
 
 //擷取app.vue放權限的router(把app地方換成router)
-router.get('/login', (req, res) => {
-    res.status(200)
-        .render('login', {
-            path: '/login',
-            pageTitle: 'Login'
-        });
-});
+// router.get('/login', (req, res) => {
+//     res.status(200)
+//         .render('login', {
+//             path: '/login',
+//             pageTitle: 'Login'
+//         });
+// });
+//對照git hub 5-1 把上面改寫成下一句
+router.get('/login', authController.getLogin);
 
-router.post('/login', (req, res) => {
-    // const { email, password } = req.body;
-    // if (email && password) {
-    //     res.redirect('/');
-    // } else {
-    //     console.log('欄位尚未填寫完成！')
-    // }
-    res.redirect('/')
-});
 
-router.post('/logout', (req, res) => {
-    res.redirect('/login');
-})
+// router.post('/login', (req, res) => {
+//      const { email, password } = req.body;
+//      if (email && password) {
+//          res.redirect('/');
+//      } else {
+//          console.log('欄位尚未填寫完成！')
+//      }
+//     res.redirect('/')
+// });
+//對照git hub 5-1 把上面改寫成下一句
+router.post('/login', authController.postLogin);
+
+// router.post('/logout', (req, res) => {
+//     res.redirect('/login');
+// })
+
+//對照git hub 5-1 把上面改寫成下一句
+router.post('/logout', authController.postLogout);
+
 
 
 module.exports=router; //匯出模組

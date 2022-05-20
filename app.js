5a104558fcbfc03edccfcbe28d93e2cb7e79550b
TzuHsienChiang <happxia@gmail.com>
2022-05-20 11:14:56 +0800

    5-7.plus 登入功能(3) 存放登入狀態在 session 中 [小挑戰]

--- app.js
+++ app.js
@@ -37,6 +37,18 @@ const oneDay = 1000 * 60 * 60 * 24; //5-7
 //   next();
 //});
 
+//5-7 先匯入express-session套件後在 app.js 匯入 express-session 套件後，就可以使用這個中介軟體函式，如下：
+app.use(session({  //前面藍字的部分都是這個session套件的規定用法
+	secret: 'sessionToken',  // 加密用的字串 
+	resave: false,   // 沒變更內容是否強制回存
+	saveUninitialized: false ,  // 新 session 未變更內容是否儲存
+	cookie: {
+		//maxAge: 10000 // session 狀態儲存多久？單位為毫秒 
+        maxAge: oneDay  //const port = 3000; const oneDay = 1000 * 60 * 60 * 24; 先宣告變數這樣以後調動數字比較方便
+	}
+})); 
+
+
 //在 HTML 使用靜態資源（img, css...）//  一定要放在路由前！！因為middleware是從上往下執行！所以要讓他們先解析資料！這樣路由才能繼續執行！
 app.use(express.static(path.join(__dirname, 'publics')));
 
@@ -46,6 +58,8 @@ app.use(bodyParser.urlencoded({ extended: false }));//因為這一行所以用�
 //5-7 接著，為了讓每一個視圖都可以使用 isLogin ，要使用 express 提供給 res 的 locals 進行儲存：儲存在 res.locals 的資料狀態，res.locals這個是＝session套組的全域用法，這樣後面的東西都可以有全域性質，而不用一個一個在需要login資料的地方(如shop.js:.render("index",{isLogin:"true"))做設定，因為這樣的話每一個都要寫屬性設定。這樣寫全部視圖可以直接取用。
 //因此，我們撰寫一個自定義的 middleware（參考老師資料夾express api寫法）將session全部存在這，撰寫方法如下：
 app.use((req, res, next) => {
+    res.locals.pageTitle = 'Book Your Books online'; //5-7
+
     res.locals.path = req.url;
     res.locals.isLogin = req.session.isLogin || false;
     next();
@@ -56,17 +70,6 @@ app.use(authRoutes);
 app.use(shopRoutes);
 app.use(errorRoutes);
 
-//5-7 先匯入express-session套件後在 app.js 匯入 express-session 套件後，就可以使用這個中介軟體函式，如下：
-app.use(session({  //前面藍字的部分都是這個session套件的規定用法
-	secret: 'sessionToken',  // 加密用的字串 
-	resave: false,   // 沒變更內容是否強制回存
-	saveUninitialized: false ,  // 新 session 未變更內容是否儲存
-	cookie: {
-		//maxAge: 10000 // session 狀態儲存多久？單位為毫秒 
-        maxAge: oneDay  //const port = 3000; const oneDay = 1000 * 60 * 60 * 24; 先宣告變數這樣以後調動數字比較方便
-
-	}
-})); 
 
 
 
@@ -169,7 +172,7 @@ app.set('views', 'views'); // 預設路徑就是 views，如果沒有變動，�
 database
 	//.sync()
     .sync({ force: true }) //5-4 修復: 資料重複 insert
-	.then((result) => {
+	.then((result) => { //因為沒有註冊功能，所以目前以這方式創立帳號，只有這帳號的人才能登入成功
         User.create({ displayName: 'Admin', email: 'admin@skoob.com', password: '11111111'})//5-6# 在啟動 Web Server 時，寫入 User 資料到資料庫中。現在，為了測試目的，在啟動 Web Server 時，使用剛建立和的 User 模組來新增 user，因為透過了 Sequelize，這個操作會同步到資料庫中：
 
         Product.bulkCreate(products);//5-3 接著，我們先在 app.js 建立 app 監聽的同時，使用 Product 來增加我們的 Product 資料。ORM 框架建立的 Product model ，擁有一個可以輸入多筆資料的方法 bulkCreate(array) 
